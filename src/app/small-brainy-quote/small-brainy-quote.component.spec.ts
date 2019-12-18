69b1096d222fae5319909f9a2caba48aab5510c0
Zarar Siddiqi <zarars@gmail.com>
2019-12-18 12:42:34 -0500

    Component for rendering small brainy quotes.

---
+++ src/app/small-brainy-quote/small-brainy-quote.component.spec.ts
@@ -0,0 +1,18 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SmallBrainyQuoteComponent } from './small-brainy-quote.component';
+import {BrainyQuotesService} from '../brainy-quotes/brainy-quotes.service';
+import {BigBrainyQuoteComponent} from '../big-brainy-quote/big-brainy-quote.component';
+
+describe('SmallBrainyQuoteComponent', () => {
+  it('should setup the brainy quotes by calling the service', () => {
+    const brainyQuotesService: BrainyQuotesService = new BrainyQuotesService();
+    const allBrainyQuotesSpy = jest.spyOn(brainyQuotesService, 'allBrainyQuotes');
+    const component: BigBrainyQuoteComponent = new BigBrainyQuoteComponent(brainyQuotesService);
+
+    component.ngOnInit();
+
+    expect(component.brainyQuotes$).not.toBeNull();
+    expect(allBrainyQuotesSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/app/small-brainy-quote/small-brainy-quote.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {BrainyQuotesService} from '../brainy-quotes/brainy-quotes.service';
+import {BrainyQuote} from '../brainy-quotes/brainy-quotes';
+import {Observable, of} from 'rxjs';
+
+@Component({
+  selector: 'app-small-brainy-quote',
+  templateUrl: './small-brainy-quote.component.html',
+  styleUrls: ['./small-brainy-quote.component.scss']
+})
+export class SmallBrainyQuoteComponent implements OnInit {
+
+  brainyQuotes$: Observable<BrainyQuote[]>;
+
+  constructor(private brainyQuotesService: BrainyQuotesService ) { }
+
+  ngOnInit() {
+    this.brainyQuotes$ = of(this.brainyQuotesService.allBrainyQuotes());
+  }
+
+}

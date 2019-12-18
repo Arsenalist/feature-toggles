33afa32a6e57b95318ccfbd1024690c21005264a
Zarar Siddiqi <zarars@gmail.com>
2019-12-18 13:09:35 -0500

    Configure routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BrainyQuotesComponent} from './brainy-quotes/brainy-quotes.component';
+import {BigBrainyQuoteComponent} from './big-brainy-quote/big-brainy-quote.component';
+import {SmallBrainyQuoteComponent} from './small-brainy-quote/small-brainy-quote.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'brainy-quotes', component: BrainyQuotesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  entryComponents: [BigBrainyQuoteComponent, SmallBrainyQuoteComponent]
 })
 export class AppRoutingModule { }

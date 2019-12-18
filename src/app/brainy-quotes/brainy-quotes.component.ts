81e0c85f409dd4407fee97d5c2133f306c7d94b8
Zarar Siddiqi <zarars@gmail.com>
2019-12-18 13:09:12 -0500

    Barebones class which just load the right component dynamically.

--- src/app/brainy-quotes/brainy-quotes.component.ts
+++ src/app/brainy-quotes/brainy-quotes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {TogglePointDirective} from '../toggle-point.directive';
+import {BrainyQuoteComponentFactory} from './brainy-quote-component-factory';
 
 @Component({
   selector: 'app-brainy-quotes',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrainyQuotesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private brainyQuoteComponentFactory: BrainyQuoteComponentFactory) { }
+
+  @ViewChild(TogglePointDirective, {static: true}) togglePoint: TogglePointDirective;
 
   ngOnInit() {
+    this.loadComponent();
   }
 
+  private loadComponent() {
+    this.togglePoint.viewContainerRef.clear();
+    this.togglePoint.viewContainerRef.createComponent(this.brainyQuoteComponentFactory.brainyQuoteComponent());
+  }
 }

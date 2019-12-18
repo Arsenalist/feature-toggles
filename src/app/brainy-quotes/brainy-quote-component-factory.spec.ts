c9cce74f9ab777027e9b67e66e4b5bdabcbf6b59
Zarar Siddiqi <zarars@gmail.com>
2019-12-18 12:44:27 -0500

    Factory method to return the right component based on feature decision.

---
+++ src/app/brainy-quotes/brainy-quote-component-factory.spec.ts
@@ -0,0 +1,54 @@
+import {BrainyQuoteComponentFactory} from './brainy-quote-component-factory';
+import {BigBrainyQuoteComponent} from '../big-brainy-quote/big-brainy-quote.component';
+import {FeatureDecisionsService} from '../feature-decisions.service';
+import {ComponentFactory, ComponentFactoryResolver, Type} from '@angular/core';
+import {SmallBrainyQuoteComponent} from '../small-brainy-quote/small-brainy-quote.component';
+
+class MockComponentFactoryResolver extends ComponentFactoryResolver {
+
+  resolveComponentFactory<T>(component: Type<T>): ComponentFactory<T> {
+    return undefined;
+  }
+}
+
+describe('BrainyQuotesService', () => {
+  it('should return BigBrainyQuotesComponent', () => {
+
+    // setup: have feature decision service return true
+    const featureDecisionsService = new FeatureDecisionsService();
+    jest.spyOn(featureDecisionsService, 'featureDecisions').mockReturnValue({
+      showLargeBrainyQuotes: true
+    });
+
+    // setup: spy on the component factory
+    const mockComponentFactoryResolver = new MockComponentFactoryResolver();
+    const resolveComponentFactorySpy = jest.spyOn(mockComponentFactoryResolver, 'resolveComponentFactory');
+
+    // execute
+    const brainy = new BrainyQuoteComponentFactory(featureDecisionsService, mockComponentFactoryResolver);
+    brainy.brainyQuoteComponent();
+
+    // verify that the right component got instantiated
+    expect(resolveComponentFactorySpy).toHaveBeenCalledWith(BigBrainyQuoteComponent);
+  });
+
+  it('should return SmallBrainyQuotesComponent', () => {
+
+    // setup: have feature decision service return false
+    const featureDecisionsService = new FeatureDecisionsService();
+    jest.spyOn(featureDecisionsService, 'featureDecisions').mockReturnValue({
+      showLargeBrainyQuotes: false
+    });
+
+    // setup: spy on the component factory
+    const mockComponentFactoryResolver = new MockComponentFactoryResolver();
+    const resolveComponentFactorySpy = jest.spyOn(mockComponentFactoryResolver, 'resolveComponentFactory');
+
+    // execute
+    const brainy = new BrainyQuoteComponentFactory(featureDecisionsService, mockComponentFactoryResolver);
+    brainy.brainyQuoteComponent();
+
+    // verify that the right component got instantiated
+    expect(resolveComponentFactorySpy).toHaveBeenCalledWith(SmallBrainyQuoteComponent);
+  });
+});

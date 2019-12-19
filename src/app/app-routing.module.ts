3b5c818eac9732b4606504125a72c09bf5cb6466
Zarar Siddiqi <zarars@gmail.com>
2019-12-18 20:01:04 -0500

    Routing for feature toggle directive demo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {BrainyQuotesContainerComponent} from './brainy-quotes/brainy-quotes-container/brainy-quotes-container.component';
 import {BigBrainyQuoteComponent} from './brainy-quotes/big-brainy-quote/big-brainy-quote.component';
 import {SmallBrainyQuoteComponent} from './brainy-quotes/small-brainy-quote/small-brainy-quote.component';
+import {MovieListComponent} from './movies/movie-list/movie-list.component';
 
 
 const routes: Routes = [
-  { path: 'brainy-quotes', component: BrainyQuotesContainerComponent }
+  { path: 'brainy-quotes', component: BrainyQuotesContainerComponent },
+  { path: 'movies', component: MovieListComponent },
 ];
 
 @NgModule({

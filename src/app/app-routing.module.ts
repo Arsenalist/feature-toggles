5adc2900c3715dde3f82a961ce328d054d6bf31e
Zarar Siddiqi <zarars@gmail.com>
2019-12-28 12:44:19 -0500

    Routing for component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import {SmallBrainyQuoteComponent} from './brainy-quotes/small-brainy-quote/smal
 import {MovieListComponent} from './movies/movie-list/movie-list.component';
 import {StockPricesComponent} from './stocks/stock-prices/stock-prices.component';
 import {CanActivateStockPrices} from './stocks/can-activate-stock-prices';
+import {NbaScoresComponent} from './scores/nba-scores/nba-scores.component';
 
 
 const routes: Routes = [
   { path: 'brainy-quotes', component: BrainyQuotesContainerComponent },
   { path: 'movies', component: MovieListComponent },
+  { path: 'nba-scores', component: NbaScoresComponent },
   {
     path: 'stock-prices',
     component: StockPricesComponent,

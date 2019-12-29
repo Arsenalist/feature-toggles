e6d28d70cd390689b0ea569619bc99991f224dbf
Zarar Siddiqi <zarars@gmail.com>
2019-12-29 18:33:29 -0500

    Entities and data.

---
+++ src/app/bookstore/bookstore-data.ts
@@ -0,0 +1,29 @@
+import {Book} from './book';
+
+export const BOOKSTORE_DATA: Book[] = [
+  {
+    title: 'Calculus, 4th edition',
+    author: 'Michael Spivak',
+    authorCountry: 'USA'
+  },
+  {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    authorCountry: 'USA'
+  },
+  {
+    title: 'Crime and Punishment',
+    author: 'Fyodor Dostoevsky',
+    authorCountry: 'Russia'
+  },
+  {
+    title: 'Dead Souls',
+    author: 'Nikolai Gogol',
+    authorCountry: 'Russia'
+  },
+  {
+    title: 'Oliver Twist',
+    author: 'Charles Dickens',
+    authorCountry: 'England'
+  }
+];

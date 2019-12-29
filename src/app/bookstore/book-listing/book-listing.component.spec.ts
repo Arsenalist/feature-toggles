0dd4190a83e64c88d76ba5e6895db3e3e5b5422b
Zarar Siddiqi <zarars@gmail.com>
2019-12-29 18:33:56 -0500

    Component class for books.

--- src/app/bookstore/book-listing/book-listing.component.spec.ts
+++ src/app/bookstore/book-listing/book-listing.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BookListingComponent } from './book-listing.component';
+import {BookListingComponent} from "./book-listing.component";
+import {FeatureAwareInvoker} from '../feature-aware-invoker';
+import {FeatureDecisionsService} from '../../feature-decisions.service';
 
 describe('BookListingComponent', () => {
-  let component: BookListingComponent;
-  let fixture: ComponentFixture<BookListingComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BookListingComponent ]
-    })
-    .compileComponents();
-  }));
+  let bookListingComponent: BookListingComponent;
+  let featureAwareInvoker: FeatureAwareInvoker;
+  let featureDecisionsService: FeatureDecisionsService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BookListingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    featureDecisionsService = new FeatureDecisionsService();
+    featureAwareInvoker = new FeatureAwareInvoker(featureDecisionsService);
+    bookListingComponent = new BookListingComponent(featureAwareInvoker);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should return books with Russian authors', () => {
+    jest.spyOn(featureDecisionsService, 'featureDecisions').mockReturnValue({
+      showBooksFromRussianAuthors: true
+    });
+
+    bookListingComponent.ngOnInit();
+
+    bookListingComponent.books$.subscribe(books => {
+      expect(books.length).toBe(5);
+      expect(books[2].authorCountry).not.toBe('Russia');
+      expect(books[2].authorCountry).toBe('Russia');
+      expect(books[3].authorCountry).toBe('Russia');
+    });
   });
+
+  it('should return books without Russian authors', () => {
+    jest.spyOn(featureDecisionsService, 'featureDecisions').mockReturnValue({
+      showBooksFromRussianAuthors: false
+    });
+
+    bookListingComponent.ngOnInit();
+
+    bookListingComponent.books$.subscribe(books => {
+      expect(books.length).toBe(3);
+      expect(books[0].authorCountry).not.toBe('Russia');
+      expect(books[1].authorCountry).not.toBe('Russia');
+      expect(books[2].authorCountry).not.toBe('Russia');
+    });
+  });
+
 });
--- src/app/bookstore/book-listing/book-listing.component.ts
+++ src/app/bookstore/book-listing/book-listing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FeatureAwareInvoker} from '../feature-aware-invoker';
+import {Observable, of} from 'rxjs';
+import {Book} from '../book';
+import {BOOKSTORE_DATA} from '../bookstore-data';
 
 @Component({
   selector: 'app-book-listing',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListingComponent implements OnInit {
 
-  constructor() { }
+  books$: Observable<Book[]>;
+
+  constructor(private featureAwareInvoker: FeatureAwareInvoker) { }
 
   ngOnInit() {
+    this.books$ = this.featureAwareInvoker.invoke(
+      'showBooksFromRussianAuthors',
+      this.withRussianAuthors,
+      this.withoutRussianAuthors,
+      BOOKSTORE_DATA);
   }
 
+  private withRussianAuthors(books: Book[]): Observable<Book[]> {
+    return of(books);
+  }
+
+  private withoutRussianAuthors(books: Book[]): Observable<Book[]> {
+    return of(books.filter( (book) => book.authorCountry !== 'Russia'));
+  }
 }

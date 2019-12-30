dc7e705184b9fefa2d4665ceadff51dca5a8c168
Zarar Siddiqi <zarars@gmail.com>
2019-12-30 18:50:31 -0500

    Move directive to app where it's used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrainyQuotesContainerComponent } from './brainy-quotes/brainy-quotes-container/brainy-quotes-container.component';
-import { TogglePointDirective } from './toggle-point.directive';
+import { TogglePointDirective } from './brainy-quotes/toggle-point.directive';
 import { BigBrainyQuoteComponent } from './brainy-quotes/big-brainy-quote/big-brainy-quote.component';
 import { SmallBrainyQuoteComponent } from './brainy-quotes/small-brainy-quote/small-brainy-quote.component';
 import { FeatureToggleDirective } from './movies/feature-toggle.directive';
